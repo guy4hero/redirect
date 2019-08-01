c9e276c9070d26996ac5f9a51348e739f8978f77
unknown <guy4hero@qq.com>
2019-08-01 10:11:07 +0800

    .

---
+++ config.js
@@ -0,0 +1,12 @@
+function func000000000000000000000000000000000000000000001() {
+    return {
+        web: {
+            ip: 'www.baidu.com',
+            port: 80
+        },
+        server: {
+            ip: '',
+            port: 8080
+        }
+    }
+}
